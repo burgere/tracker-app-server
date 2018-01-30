dfc1674490f6650fa0b8425834f10ca93d50a800
burgere <eric.burger@comcast.net>
2018-01-30 07:34:03 -0600

    people endpoint

--- routes/people.js
+++ routes/people.js
@@ -25,27 +25,33 @@ var config = require('../config');
               }
             });
      }
+
+     function retrieveStoriesByUser(userId, stories) {
+         return _.filter(stories, (story) => {
+             return story.owner_ids.includes(userId)
+         });
+     }
     
     axios.all([getUsers(), getStories()])
         .then(axios.spread((usersResponse, storiesResponse) => {
             let users = []
             let filteredUserResponseData = _.filter(usersResponse.data, (person) => {
-                return person["role"] === "member"   
+                return person["role"] === "member"  
             });
 
             for(var i = 0; i < filteredUserResponseData.length; i++) {
                 users.push({
                     key: filteredUserResponseData[i].person.name,
                     user_id: filteredUserResponseData[i].person.id,
-                    stories: []
+                    stories: retrieveStoriesByUser(filteredUserResponseData[i].person.id, storiesResponse.data)
                 });
             }
 
-            for(var i=0; i < storiesResponse.data.length; i++) {
-                let user = _.findWhere(users, {id: storiesResponse.data[i]})
-            }
+            let usersWithStories = _.filter(users, (user) => {
+                return user.stories.length > 0
+            });
 
-            res.json(JSON.parse(circularJson.stringify(users)));
+            res.json(JSON.parse(circularJson.stringify(usersWithStories)));
 
     }))
 
